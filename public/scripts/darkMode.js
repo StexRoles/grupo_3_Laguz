00869776494fb49c019d93057830d79f95aaf16d
nachorsc <nacho.rodriguez162@gmail.com>
2024-02-28 23:43:09 -0300

    Agregando carpeta scripts

---
+++ public/scripts/darkMode.js
@@ -0,0 +1,8 @@
+window.addEventListener('load',()=>{
+    let buttonDark = document.querySelector('#switch');
+    buttonDark.addEventListener('click',()=>{
+        buttonDark.classList.toggle('active');
+    })
+})
+
+
