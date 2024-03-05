fb8200a50507aac7fa48ba1d637aeedbb2a751c2
nachorsc <nacho.rodriguez162@gmail.com>
2024-03-04 23:05:48 -0300

    Agregando Storage para mantener posicion boton DarkMode

--- public/scripts/darkMode.js
+++ public/scripts/darkMode.js
@@ -1,8 +1,19 @@
-window.addEventListener('load',()=>{
+window.addEventListener('load', () => {
     let buttonDark = document.querySelector('#switch');
-    buttonDark.addEventListener('click',()=>{
-        buttonDark.classList.toggle('active');
-    })
-})
+    
+    // Verificar si hay un estado almacenado en localStorage
+    const isDarkMode = localStorage.getItem('darkMode') === 'true';
+    
+    // Aplicar la clase 'active' si el modo oscuro está activado
+    if (isDarkMode) {
+        buttonDark.classList.add('active');
+    }
 
+    buttonDark.addEventListener('click', () => {
+        buttonDark.classList.toggle('active');
+        
+        // Guardar el estado en localStorage
+        localStorage.setItem('darkMode', buttonDark.classList.contains('active'));
+    });
+});
 
