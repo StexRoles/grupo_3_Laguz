ab8d4c48d5e83eaa4f7359a8a5cb918aa27b7896
EzeLazzara <elazzara0201@gmail.com>
2024-03-19 15:24:31 -0300

    Agregando validaciones de registro

--- src/middlewares/validateRegisterMiddleware.js
+++ src/middlewares/validateRegisterMiddleware.js
@@ -3,9 +3,9 @@ const { body } = require('express-validator');
 
 // HACER LAS VALIDACIONES
 const validations = [
-    body('user').notEmpty().withMessage('Debes completar el campo de usuario'),
+    body('user').notEmpty().withMessage('Debes completar el campo de usuario') .isLength ({  min: 2 }) .withMessage("El nombre de usuario debe tener como minimo 2 caracteres"),
     body('email').notEmpty().withMessage('Debes completar el campo de email').bail().isEmail().withMessage('Debes completar con un email válido'),
-    body('password').notEmpty().withMessage('Debes completar el campo de contraseña'),
+    body('password').notEmpty().withMessage('Debes completar el campo de contraseña') .isLength ({  min: 8 }) .withMessage("La contraseña debe tener como minimo 8 caracteres"),
     body('repassword').notEmpty().withMessage('Debes completar el campo de confirmar contraseña'),
     body('repassword').custom((value, { req }) => {
         if (value !== req.body.password) {
